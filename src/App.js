6127a4f61732f787967d148405ab1d5ea1602048
Michał Niemiec <michal.niemiec@billennium.com>
2021-11-29 03:21:43 +0100

    Lazy routes - dynamic reducers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 
 import { store } from './store';
 import Layout from "./components/Layout";
-import Characters from "./containers/Characters";
-import Planets from "./containers/Planets";
-import Starships from "./containers/Starships";
+
+const Characters = lazy(() => import("./containers/Characters"))
+const Planets = lazy(() => import("./containers/Planets"))
+const Starships = lazy(() => import("./containers/Starships"))
+
+const Fallback = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: yellow;
+`
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<></>} />
-          <Route path="characters" element={<Characters />} />
-          <Route path="planets" element={<Planets />} />
-          <Route path="starships" element={<Starships />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Fallback />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="characters" element={<Characters />} />
+            <Route path="planets" element={<Planets />} />
+            <Route path="starships" element={<Starships />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </Provider>
 )
